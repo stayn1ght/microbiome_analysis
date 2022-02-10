0f5143db59109d3d65b24459924a453b3bfd9049
WangM <74388983+goodTHEyouth@users.noreply.github.com>
2022-02-10 17:17:58 +0800

    Update and rename plotly.R to prevalence.R

--- interactive visualization by R/plotly.R
+++
@@ -1,1 +0,0 @@
-# use package 'plotly' to draw interactive graph
---
+++ interactive visualization by R/prevalence.R
@@ -0,0 +1,7 @@
+# use package 'plotly' to draw interactive graph
+library(plotly)
+
+## 计算物种的相对丰度的均值与中位数和流行度
+### 流行度：物种在多少样本中存在。在样本中某物种相对丰度高于某个阈值，比如0.01%，我们认为这个物种在这个样本中是存在的。
+
+## 用plotly绘制交互式散点图
