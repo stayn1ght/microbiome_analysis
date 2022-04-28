c997cedba3a34dd64cf8802965b07f820bbd6afd
WangM <74388983+stayn1ght@users.noreply.github.com>
2022-04-28 10:20:50 +0800

    Create qiime2_for_merged_reads.sh

---
+++ qiime2_analyse/qiime2_for_merged_reads.sh
@@ -0,0 +1,3 @@
+# using PRJDB7616 as an example
+# In this bioproject, the downloaded sequence from NCBI Bioproject seems to be merged reads, with the whole length longer than 900bp. 
+# Because of the very high quanlity of these sequences and that the sequences are merged reads, denoising steps should be eliminished.
