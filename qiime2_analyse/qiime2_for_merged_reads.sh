b7238a0249bdea0102d7b872d3078f1b85a393f4
WangM <74388983+stayn1ght@users.noreply.github.com>
2022-04-30 16:38:10 +0800

    Update qiime2_for_merged_reads.sh

--- qiime2_analyse/qiime2_for_merged_reads.sh
+++ qiime2_analyse/qiime2_for_merged_reads.sh
@@ -4,19 +4,18 @@
 
 # above all, using the scripts in qiime2_pipeline.md to import data as qza file.
 
-qiime vsearch dereplicate-sequences \
+time qiime vsearch dereplicate-sequences \
   --i-sequences 03_qiime_results/02_single-end-demux.qza \
   --o-dereplicated-table 03_qiime_results/03_table.qza \
   --o-dereplicated-sequences 03_qiime_results/04_rep-seqs.qza
 
 # next step is de novo clustering of the sequences, aiming to make it faster to classify.
-qiime vsearch cluster-features-de-novo \
+time qiime vsearch cluster-features-de-novo \
   --i-table 03_qiime_results/03_table.qza \
   --i-sequences 03_qiime_results/04_rep-seqs.qza \
   --p-perc-identity 0.99 \
   --o-clustered-table 03_qiime_results/table-dn-99.qza \
   --o-clustered-sequences 03_qiime_results/rep-seqs-dn-99.qza
-
 
 time qiime feature-classifier classify-sklearn \
   --i-classifier /mnt/raid7/mingyuwang/gut_fungus/example_PRJNA751473_ITS/02_classifier/01* \
